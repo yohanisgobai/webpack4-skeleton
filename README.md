2330186025598fa87e69a7e33add61964c605ee2
yoolabs <yoolabs@yoolabss-MacBook-Pro.local>
2018-07-17 21:50:18 +0200

    Simple Configuration with some basic Loader

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Webpack skeleton
+
+Getting started with Loader Configuration:
+1. CSS loader
+2. Image loader
+3. File Loader(JSON(by Default), CSV, XML)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 "use strict";
 
-import _ from "lodash";
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon-gr.png';
 
 function component() {
   var element = document.createElement('div');
 
   //Lodash with import module
   element.innerHTML = _.join(['Hello', 'webpack','changed'], ' ');
+  element.classList.add('hello');
+
+  //Add Icon
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
